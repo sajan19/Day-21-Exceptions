d04d48e64b2e28e577c96844ff359e2bfc9ef471
Sajan <sajanmhatre29@gmail.com>
2021-09-16 17:38:43 +0530

    Null Point Exception

--- MoodAnalyser/src/main/java/com/MoodAnalyser/ModeAnalyser.java
+++ MoodAnalyser/src/main/java/com/MoodAnalyser/ModeAnalyser.java
@@ -1,10 +1,18 @@
 package com.MoodAnalyser;
 
 public class ModeAnalyser {
+    String message;
+    //Method to Analyse User Mood
     public String analyseMood(String message) {
-        if (message.contains("SAD"))
-        return "SAD";
-        else
+        try{
+            if (message.contains("SAD"))
+                return "SAD";
+            else
+                return "HAPPY";
+        }
+        //Handling Null Point Exception
+        catch(NullPointerException e) {
             return "HAPPY";
+        }
     }
 }
--- MoodAnalyser/src/test/java/com/MoodAnalyser/MoodAnalyserTest.java
+++ MoodAnalyser/src/test/java/com/MoodAnalyser/MoodAnalyserTest.java
@@ -4,17 +4,25 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class MoodAnalyserTest {
+    //Test to check Sad Mood
     @Test
     void givenMessage_whenSad_ShouldReturnSad() {
         ModeAnalyser modeAnalyser =new ModeAnalyser();
         String mood = modeAnalyser.analyseMood("This is SAD Message");
         Assertions.assertEquals("SAD",mood);
     }
-
+    //Test to check Happy Mood
     @Test
     void givenMessage_whenNotSad_ShouldReturnHappy() {
         ModeAnalyser modeAnalyser =new ModeAnalyser();
         String mood = modeAnalyser.analyseMood("This is HAPPY Message");
+        Assertions.assertEquals("HAPPY",mood);
+    }
+    //Test to check Null Mood (Null Point Exception)
+    @Test
+    void givenNullMood_ShouldReturnHappy() {
+        ModeAnalyser modeAnalyser =new ModeAnalyser();
+        String mood = modeAnalyser.analyseMood(null);
         Assertions.assertEquals("HAPPY",mood);
     }
 }
